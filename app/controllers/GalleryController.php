d4a820de2b243b9b976b513c1649d21c3bf29653
Olof Haglund <olof@olofhaglund.name>
2017-05-09 13:54:59 +0200

    Added to better way to find right path

--- app/controllers/GalleryController.php
+++ app/controllers/GalleryController.php
@@ -15,10 +15,10 @@ class GalleryController extends BaseController{
     }
     public function create(){
         $data = Input::only(['name']);
-        if(is_dir('img/gallery/' . $data['name'])){
+        if(is_dir(public_path() . '/img/gallery' . $data['name'])){
             return Redirect::back()->withInput()->withErrors("Foulder with name allready exists");
         }
-        $success = mkdir('img/gallery/' . $data['name'], 0777);
+        $success = mkdir(public_path() . '/img/gallery' . $data['name'], 0777);
         if($success){
             return Redirect::to('gallery/' . $data['name']);
         }
